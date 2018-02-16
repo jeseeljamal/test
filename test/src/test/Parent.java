b0777eb63c3d306d706627b266027ac900491d70
jeseel jamal <jeseel jamal>
2018-02-16 10:38:25 +0530

    v3

--- test/src/test/Parent.java
+++ test/src/test/Parent.java
@@ -3,9 +3,15 @@ package test;
 public class Parent {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 	System.out.println("testA");
 	System.out.println("testB");
 	System.out.println("testC");
+=======
+	System.out.println("test4");
+	System.out.println("test5");
+	System.out.println("test6");
+>>>>>>> 7b6c06c... v3
 	}
 
 }
